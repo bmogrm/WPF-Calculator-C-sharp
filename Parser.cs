d5e73fa34e9de7a6723139722b10758b5bebaade
Rodion Skorba <r.skorba@mail.ru>
2023-11-12 13:05:53 +0500

    The basis for the application logic has been create

---
+++ Parser.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace wpfCalculated
+{
+    public class Calculator
+    {
+        public int i = 0;
+        public string s = "";
+
+        public double ProcE() //результат 
+        {
+            s += '\0';
+            double x = ProcT();
+            while (s[i] == '+' || s[i] == '-')
+            {
+                char p = s[i];
+                i++;
+                if (p == '+')
+                {
+                    x += ProcT();
+                }
+                else
+                {
+                    x -= ProcT();
+                }
+            }
+            i = 0;
+            return x;
+        }
+        public double ProcT()
+        {
+            double x = ProcM();
+            while (s[i] == '*' || s[i] == '/')
+            {
+                char p = s[i]; i++;
+                if (p == '*')
+                {
+                    x *= ProcM();
+                }
+                else
+                {
+                    x /= ProcM();
+                }
+            }
+            return x;
+        }
+        public double ProcM()
+        {
+            double x = 0;
+            if (s[i] == '(')
+            {
+                i++;
+                x = ProcE(); if (s[i] != ')')
+                {
+                    Console.WriteLine("missing ')'");
+                }
+                i++;
+            }
+            else
+            {
+                if (s[i] == '-')
+                {
+                    i++;
+                    x -= ProcM();
+                }
+                else
+                {
+                    if (s[i] >= '0' && s[i] <= '9')
+                    {
+                        x = ProcC();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Syntex error.");
+                    }
+                }
+            }
+            return x;
+        }
+        public double ProcC()
+        {
+            double x = 0;
+            double y = 0, c = 0;
+            while (s[i] >= '0' && s[i] <= '9')
+            {
+                x *= 10;
+                x += s[i] - '0';
+                i++;
+            }
+            if (s[i] == ',')
+            {
+                i++;
+                while(s[i] >= '0' && s[i] <= '9')
+                {
+                    y *= 10;
+                    y += s[i] - '0';
+                    i++;
+                    c++;
+                }
+                if(c != 0)
+                {
+                    x = x + y / Math.Pow(10, c);
+                }
+            }
+            return x;
+        }
+    }
+}
+
---
+++ ViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfCalculated
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly Calculator _model;
+        private string _result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public ICommand calculateCommand { get; }
+        public ICommand addToTextBoxCommand { get; }
+        public ICommand clearCommand { get; }
+        public ICommand clearOnceCommand { get; }
+
+        public ViewModel()
+        {
+            _model = new Calculator();
+            calculateCommand = new RelayCommand(ExecuteCalculate);
+            addToTextBoxCommand = new RelayCommand(ExecuteAdd);
+            clearCommand = new RelayCommand(ExecuteClear);
+            clearOnceCommand = new RelayCommand(ExecuteClearOnce);
+        }
+
+        private void ExecuteAdd(object parameter)
+        {
+            if (!string.IsNullOrEmpty(Result))
+            {
+                if (Result.Contains('='))
+                {
+                    Result = string.Empty;
+                }
+            }
+            string number = parameter as string;
+            Result += number;
+        }
+
+        private void ExecuteCalculate(object parameter)
+        {
+            if (!string.IsNullOrEmpty(Result))
+            {
+                _model.s = Result;
+                Result =  _model.ProcE().ToString();
+                AddToHistory(_model.s + " = " + Result);
+            }
+        }
+
+        private void ExecuteClear(object parameter)
+        {
+            Result = string.Empty;
+        }
+
+        private void ExecuteClearOnce(object parameter)
+        {
+            Result = Result.Remove(Result.Length - 1);
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<string> _calculationHistory = new ObservableCollection<string>();
+        public ObservableCollection<string> CalculationHistory
+        {
+            get { return _calculationHistory; }
+            set
+            {
+                _calculationHistory = value;
+                OnPropertyChanged(nameof(CalculationHistory));
+            }
+        }
+
+        public void AddToHistory(string calculation)
+        {
+            if (CalculationHistory.Count == 5)
+            {
+                CalculationHistory.RemoveAt(0);
+            }
+            CalculationHistory.Add(calculation);
+            OnPropertyChanged(nameof(CalculationHistory));
+        }
+    }
+
+
+
+
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
